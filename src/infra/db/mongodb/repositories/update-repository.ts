2f1312deedc7728c68868dc5c2a81bb9801b1dc1
Willian Cavalcanti Coelho <willian.cacoelho@gmail.com>
2023-10-01 16:47:35 -0300

    feat: ensure mongo update works

---
+++ src/infra/db/mongodb/repositories/update-repository.ts
@@ -0,0 +1,27 @@
+import { ObjectId } from "mongodb";
+import type {
+  UpdateRepository,
+  UpdateRepositoryInput,
+} from "../../../../data/protocols/update-repository";
+import type { TaskModel } from "../../../../domain/models/task";
+import { mongoHelper } from "../helpers/mongo-helper";
+
+export class MongoUpdateRepository implements UpdateRepository {
+  public async update({
+    id,
+    ...dataToUpdate
+  }: UpdateRepositoryInput): Promise<TaskModel> {
+    const taskCollection = await mongoHelper.getCollection("task");
+
+    const { upsertedId } = await taskCollection.updateOne(
+      {
+        _id: new ObjectId(id),
+      },
+      dataToUpdate
+    );
+
+    return taskCollection.findOne<TaskModel>({
+      _id: upsertedId,
+    });
+  }
+}
