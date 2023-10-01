000bc09e3ed4cb268d43c1adbcedd34f14b39741
Willian Cavalcanti Coelho <willian.cacoelho@gmail.com>
2023-10-01 15:19:47 -0300

    feat: ensure save repository works

---
+++ src/infra/db/mongodb/repositories/save-repository.ts
@@ -0,0 +1,21 @@
+import type {
+  SaveRepository,
+  SaveRepositoryInput,
+} from "../../../../data/protocols/save-repository";
+import type { TaskModel } from "../../../../domain/models/task";
+import { mongoHelper } from "../helpers/mongo-helper";
+
+export class MongoSaveRepository implements SaveRepository {
+  public async save(data: SaveRepositoryInput): Promise<TaskModel> {
+    const taskCollection = await mongoHelper.getCollection("task");
+
+    const { insertedId } = await taskCollection.insertOne({
+      data,
+      finished: false,
+    });
+
+    return taskCollection.findOne<TaskModel>({
+      _id: insertedId,
+    });
+  }
+}
