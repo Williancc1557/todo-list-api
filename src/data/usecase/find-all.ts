7c6a15d086e566cde2159e8b41f3453fdc60062a
Willian Cavalcanti Coelho <willian.cacoelho@gmail.com>
2023-10-04 23:47:41 -0300

    refactor: change type

--- src/data/usecase/find-all.ts
+++ src/data/usecase/find-all.ts
@@ -1,12 +1,11 @@
 import type { TaskModel } from "../../domain/models/task";
-import type { FindAll } from "../../domain/usecase/find-all";
-import type { FindOneInput } from "../../domain/usecase/find-one";
+import type { FindAll, FindAllInput } from "../../domain/usecase/find-all";
 import type { FindRepository } from "../protocols/find-repository";
 
 export class DbFindAll implements FindAll {
   public constructor(private readonly findRepository: FindRepository) {}
 
-  public async find(data: FindOneInput): Promise<Array<TaskModel>> {
+  public async find(data: FindAllInput): Promise<Array<TaskModel>> {
     return this.findRepository.findAll({
       userId: data.userId,
     });
