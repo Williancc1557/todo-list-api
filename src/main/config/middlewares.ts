cf60ab78c7ec77a90eb827c41f6ffdbcca425223
Willian Cavalcanti Coelho <willian.cacoelho@gmail.com>
2023-10-06 11:07:22 -0300

    feat: implements rate limit

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -4,11 +4,13 @@ import { contentType } from "../middlewares/content-type";
 import { cors } from "../middlewares/cors";
 import { verifyAccess } from "../middlewares/verify-access";
 import { passContentHeaderMiddleware } from "../middlewares/pass-content-header";
+import { limiter } from "../middlewares/rate-limit";
 
 export const setupMiddlewares = (app: Express) => {
   app.use(bodyParser);
   app.use(contentType);
   app.use(cors);
+  app.use(limiter);
   app.use(verifyAccess);
   app.use(passContentHeaderMiddleware);
 };
